9757ce915e97acce438a8455eaae75b9f340d699
Bruno <Bruno@192.168.1.128>
2013-11-08 13:18:10 +0000

    kata4 primera version

---
+++ src/kata4/MainFrame.java
@@ -0,0 +1,67 @@
+package kata4;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class MainFrame extends JFrame{
+    
+    public MainFrame() throws HeadlessException{
+        this.setTitle("Money Calculator");
+        this.setMinimumSize(new Dimension(300, 300));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        createComponents();
+        this.setVisible(true);
+    }
+
+    private void createComponents() {
+        this.add(crateToolbar(), BorderLayout.SOUTH);
+    }
+
+    private JPanel crateToolbar() {
+        JPanel toolbar = new JPanel();
+        toolbar.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        toolbar.add(createCalculateButton());
+        toolbar.add(createExitButton());
+        return toolbar;
+    }
+
+    private JButton createCalculateButton() {
+        JButton button = new JButton("Calculate");
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                calculate();
+            }
+        });
+        return button;
+    }
+
+    private JButton createExitButton() {
+        JButton button = new JButton("Exit");
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                exit();
+            }
+        });
+        return button;
+    }
+    
+    private void exit() {
+        dispose();
+    }
+    
+    private void calculate() {
+        System.out.println("Calculating...");
+    }
+}
---
+++ src/kata4/MoneyPanel.java
@@ -0,0 +1,19 @@
+package kata4;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class MoneyPanel extends JPanel {
+
+    public MoneyPanel() {
+        this.add(createTextField());
+        this.add(new CurrencyPanel());
+    }
+
+    private JTextField createTextField() {
+        JTextField textField = new JTextField(10);
+        return textField;
+    }
+    
+    
+}
